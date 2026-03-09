c0b83dba7c011f4d57d2aeef4a686ad86b0b567d
Archie Roach <archie.roach@esh.uk.com>
2026-03-09 22:58:26 +0000

    disable exhaustive-deps eslint rule

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -8,7 +8,7 @@ module.exports = {
       plugins: ['react-hooks'],
       rules: {
         'react-hooks/rules-of-hooks': 'error',
-        'react-hooks/exhaustive-deps': 'warn',
+        'react-hooks/exhaustive-deps': 'off',
       },
     },
   },
